3dcb08d7c33a76b2fd3d5a43df5f343f78883d63
Denis <Siinthd@protonmail.com>
2019-06-03 12:21:54 +0300

    CRC

--- ModbusRTU/Source.cpp
+++ ModbusRTU/Source.cpp
@@ -26,3 +26,23 @@ void printPackage(char* data, int size, int isin)
 	printf("\n\r");
 
 }
+
+uint16_t ModRTU_CRC(byte* buf, int len)
+{
+	uint16_t crc = 0xFFFF;
+
+	for (int pos = 0; pos < len; pos++) {
+		crc ^= (uint16_t)buf[pos];          // XOR byte into least sig. byte of crc
+
+		for (int i = 8; i != 0; i--) {    // Loop over each bit
+			if ((crc & 0x0001) != 0) {      // If the LSB is set
+				crc >>= 1;                    // Shift right and XOR 0xA001
+				crc ^= 0xA001;
+			}
+			else                            // Else LSB is not set
+				crc >>= 1;                    // Just shift right
+		}
+	}
+	// Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+	return crc;
+}
