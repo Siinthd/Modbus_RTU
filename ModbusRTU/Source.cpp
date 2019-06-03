7f03ce7dc0678a058a50963d439cd240cc082be3
Denis <Siinthd@protonmail.com>
2019-06-03 12:04:45 +0300

    Debug info

--- ModbusRTU/Source.cpp
+++ ModbusRTU/Source.cpp
@@ -10,3 +10,19 @@ struct requestSingle {
 	byte Slave_code[8];
 };
 
+void printPackage(requestSingle* data, int size, int isin)
+{
+	printf("%s bytes: %d\n\r\t", (isin) ? "Received" : "Sent", size);
+	for (int i = 0; i < size; i++)
+		printf("%02X ", data->Slave_code[i]);
+	printf("\n\r");
+}
+
+void printPackage(char* data, int size, int isin)
+{
+	printf("%s bytes: %d\n\r\t", (isin) ? "Received" : "Sent", size);
+	for (int i = 0; i < size; i++)
+		printf("%02X ", (byte)data[i]);
+	printf("\n\r");
+
+}
