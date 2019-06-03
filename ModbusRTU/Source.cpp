08e002d595908a41c7bcb757dc8593379e60b67a
Denis <Siinthd@protonmail.com>
2019-06-03 11:32:00 +0300

    new

---
+++ ModbusRTU/Source.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <malloc.h>
+
+struct requestSingle {
+	byte Slave_code[8];
+};
+
